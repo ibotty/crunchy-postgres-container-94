ca276c62071cbdb65f11e4c7306e0453ef51dea0
jmccormick2001 <jeff.mccormick@crunchydata.com>
2015-12-03 14:51:42 -0600

    added some sample scripts to run a master-standby configuration

---
+++ run-pg-master.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+
+# Copyright 2015 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "starting pg-master container..."
+DATA_DIR=/tmp/pg-master-data
+sudo rm -rf $DATA_DIR
+sudo mkdir -p $DATA_DIR
+sudo chown postgres:postgres $DATA_DIR
+sudo chcon -Rt svirt_sandbox_file_t $DATA_DIR
+sudo docker run \
+	-p 12000:5432 \
+	-e TEMP_BUFFERS=9MB \
+	-e MAX_CONNECTIONS=101 \
+	-e SHARED_BUFFERS=129MB \
+	-e MAX_WAL_SENDERS=7 \
+	-e WORK_MEM=5MB \
+	-e PG_MODE=master \
+	-e PG_MASTER_USER=masteruser \
+	-e PG_MASTER_PASSWORD=masterpsw \
+	-e PG_USER=testuser \
+	-e PG_ROOT_PASSWORD=rootpsw \
+	-e PG_PASSWORD=testpsw \
+	-e PG_DATABASE=testdb \
+	--name=pg-master -d -v $DATA_DIR:/pgdata crunchydata/crunchy-pg:latest
+
